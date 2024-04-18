c12c2d4b4a6602c42fc98bf37055ce7903d91afb
miyachun <miyachun808@gmail.com>
2024-04-18 08:48:28 +0800

    init

---
+++ flask_restful/app06_01.py
@@ -0,0 +1,60 @@
+$ curl http://localhost:5000/todos
+{"todo1": {"task": "build an API"}, "todo3": {"task": "profit!"}, "todo2": {"task": "?????"}}
+
+
+$ curl http://localhost:5000/todos/todo3
+{"task": "profit!"}
+
+
+$ curl http://localhost:5000/todos/todo2 -X DELETE -v
+
+> DELETE /todos/todo2 HTTP/1.1
+> User-Agent: curl/7.19.7 (universal-apple-darwin10.0) libcurl/7.19.7 OpenSSL/0.9.8l zlib/1.2.3
+> Host: localhost:5000
+> Accept: */*
+>
+* HTTP 1.0, assume close after body
+< HTTP/1.0 204 NO CONTENT
+< Content-Type: application/json
+< Content-Length: 0
+< Server: Werkzeug/0.8.3 Python/2.7.2
+< Date: Mon, 01 Oct 2012 22:10:32 GMT
+
+
+$ curl http://localhost:5000/todos -d "task=something new" -X POST -v
+
+> POST /todos HTTP/1.1
+> User-Agent: curl/7.19.7 (universal-apple-darwin10.0) libcurl/7.19.7 OpenSSL/0.9.8l zlib/1.2.3
+> Host: localhost:5000
+> Accept: */*
+> Content-Length: 18
+> Content-Type: application/x-www-form-urlencoded
+>
+* HTTP 1.0, assume close after body
+< HTTP/1.0 201 CREATED
+< Content-Type: application/json
+< Content-Length: 25
+< Server: Werkzeug/0.8.3 Python/2.7.2
+< Date: Mon, 01 Oct 2012 22:12:58 GMT
+<
+* Closing connection #0
+{"task": "something new"}
+
+
+$ curl http://localhost:5000/todos/todo3 -d "task=something different" -X PUT -v
+
+> PUT /todos/todo3 HTTP/1.1
+> Host: localhost:5000
+> Accept: */*
+> Content-Length: 20
+> Content-Type: application/x-www-form-urlencoded
+>
+* HTTP 1.0, assume close after body
+< HTTP/1.0 201 CREATED
+< Content-Type: application/json
+< Content-Length: 27
+< Server: Werkzeug/0.8.3 Python/2.7.3
+< Date: Mon, 01 Oct 2012 22:13:00 GMT
+<
+* Closing connection #0
+{"task": "something different"}
