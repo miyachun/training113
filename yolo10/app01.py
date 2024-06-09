99846a39e8cfe76c2ec05ad360f69d4682bea6cf
miyachun <miyachun808@gmail.com>
2024-06-09 16:54:04 +0800

    init

--- yolo10/app01.py
+++ yolo10/app01.py
@@ -2,8 +2,8 @@ from ultralytics import YOLO
 from ultralytics.solutions import object_counter
 import cv2
 
-model = YOLO("yolov8n.pt")
-cap = cv2.VideoCapture("videoaa.mp4")
+model = YOLO("last.pt")
+cap = cv2.VideoCapture("videos.mp4")
 assert cap.isOpened(), "Error reading video file"
 w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
 
@@ -11,7 +11,7 @@ w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FR
 region_points = [(20, 400), (1080, 404), (1080, 360), (20, 360)]
 
 # Video writer
-video_writer = cv2.VideoWriter("videoaa.avi",
+video_writer = cv2.VideoWriter("videos.avi",
                        cv2.VideoWriter_fourcc(*'mp4v'),
                        fps,
                        (w, h))
