206db7d1ba5ca9515cbfe18561812a847c9183f0
miyachun <miyachun808@gmail.com>
2024-06-21 17:09:46 +0800

    init

--- yolov8n/app07.py
+++ yolov8n/app07.py
@@ -3,11 +3,11 @@ import cv2
 from ultralytics import YOLO, solutions
 
 model = YOLO("yolov8n-pose.pt")
-cap = cv2.VideoCapture("girl.mp4")
+cap = cv2.VideoCapture("people.mp4")
 assert cap.isOpened(), "Error reading video file"
 w, h, fps = (int(cap.get(x)) for x in (cv2.CAP_PROP_FRAME_WIDTH, cv2.CAP_PROP_FRAME_HEIGHT, cv2.CAP_PROP_FPS))
 
-video_writer = cv2.VideoWriter("workouts.avi", cv2.VideoWriter_fourcc(*"mp4v"), fps, (w, h))
+video_writer = cv2.VideoWriter("people.avi", cv2.VideoWriter_fourcc(*"mp4v"), fps, (w, h))
 
 gym_object = solutions.AIGym(
     line_thickness=2,
